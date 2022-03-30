e420dcfa65c8504b7caa25bdd1b3fd872a8bd3a4
thao02122002 <vant16039@gamil.com>
2022-03-30 22:50:27 +0700

    feat

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,6 +6,8 @@ const Banner = (props: Props) => {
   return (
     <div className='py-4 px-4'>
       <img src='https://theme.hstatic.net/200000066044/1000820067/14/slider_1.jpg?v=98' width='100%'/>
+
+      
     </div>
   )
 }
