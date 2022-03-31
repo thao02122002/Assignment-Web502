7b7fe16ddc8550f61b30d004b6fa5c4792650023
thao02122002 <vant16039@gamil.com>
2022-03-31 14:57:41 +0700

    new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,36 +8,39 @@ const Footer = (props: Props) => {
       <div className='grid grid-cols-12 gap-3 p-4 pl-8 text-left'>
 
         <div className='col-span-4'>
-        <h1 className=''><strong>Về chúng tôi</strong></h1>
+          <h1 className=''><strong>Về chúng tôi</strong></h1>
           <div className=''><img src='https://theme.hstatic.net/200000066044/1000820067/14/logo-footer.png?v=98' width='200px' /></div>
-          
+
           <p className='text-xl'><strong>MỸ PHẨM CHÍNH HÃNG NGOẠI NHẬP</strong></p>
           <div><span>Địa chỉ:</span> <span>Trường Cao đẳng FPT Polytecnic Hà Nội</span></div>
           <div><span>SĐT:</span> < span>0392697014</span></div>
           <span>Email:</span><span>thaovtpph16945@fpt.edu.vn</span>
           <div className='grid grid-cols-4'>
             <div className='col-span-1'>
-              <a href="https://www.facebook.com/profile.php?id=100069569706340"><img src='https://theme.hstatic.net/200000066044/1000820067/14/facebook.svg?v=98' width='30px'/></a>
+              <a href="https://www.facebook.com/profile.php?id=100069569706340"><img src='https://theme.hstatic.net/200000066044/1000820067/14/facebook.svg?v=98' width='30px' /></a>
             </div>
             <div className='col-span-3'>
-            <a href="https://www.facebook.com/profile.php?id=100069569706340"><img src='https://theme.hstatic.net/200000066044/1000820067/14/instagram.svg?v=98' width='30px'/></a>
+              <a href="https://www.facebook.com/profile.php?id=100069569706340"><img src='https://theme.hstatic.net/200000066044/1000820067/14/instagram.svg?v=98' width='30px' /></a>
             </div>
           </div>
         </div>
         <div className='col-span-4'>
           <h1 className=''><strong>Liên hệ</strong></h1>
           <div className=' pr-16'>
-             <p>Trang chủ</p>
-          <p>Sản phẩm</p>
-          <p>Liên hệ</p>
-          <p>Tin tức</p>
+            <p>Trang chủ</p>
+            <p>Sản phẩm</p>
+            <p>Liên hệ</p>
+            <p>Tin tức</p>
           </div>
-         
+
         </div>
 
         <div className='col-span-4'>
-          
-         
+          <h1>Fanpage</h1>
+          <div>
+            <iframe width="350px" height="250px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameBorder={0} scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v2.12/plugins/page.php?adapt_container_width=false&app_id=&channel=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df6dd0e6cd1ba98%26domain%3Dshopsonmoi.vn%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fshopsonmoi.vn%252Ff1f81aa56aa1c4%26relation%3Dparent.parent&container_width=350&height=250&hide_cover=false&href=https%3A%2F%2Fwww.facebook.com%2FShopSonMoiVietNam&locale=en_US&sdk=joey&show_facepile=true&show_posts=false&small_header=false&width=350" style={{ border: 'none', visibility: 'visible', width: 350, height: 130 }} />
+          </div>
+
         </div>
 
       </div>
--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -7,8 +7,8 @@ type ProductListProps = {
 
 const ListProduct = ({ products }: ProductListProps) => {
   return (
-    
-    <div className='py-8 px-4'>
+    <div className='p-4' >
+      <div className='py-8 '>
       <div className='border border-pink-400 bg-pink-400'>
         <div className='py-2 px-8'>
           <div className='border border-white bg-white  rounded-lg'>
@@ -43,6 +43,58 @@ const ListProduct = ({ products }: ProductListProps) => {
       </div>
 
     </div>
+
+    <div className='py-4'>
+      <h1 className='text-left hover:text-pink-400'>THƯƠNG HIỆU NỔI BẬT</h1>
+      <div className='grid grid-cols-10 gap-2'>
+        <div className='col-span-2'>
+          <img src='https://theme.hstatic.net/200000066044/1000820067/14/brand_1.jpg?v=98' width='300px'/>
+        </div>
+        <div className='col-span-2 pt-8'>
+          <img src='https://theme.hstatic.net/200000066044/1000820067/14/brand_4.jpg?v=98' width='300px'/>
+        </div>
+        <div className='col-span-2'>
+          <img src='https://theme.hstatic.net/200000066044/1000820067/14/brand_7.jpg?v=98' width='300px'/>
+        </div>
+        <div className='col-span-2'>
+          <img src='https://theme.hstatic.net/200000066044/1000820067/14/brand_2.jpg?v=98' width='300px'/>
+        </div>
+        <div className='col-span-2'>
+          <img src='https://theme.hstatic.net/200000066044/1000820067/14/brand_6.jpg?v=98' width='300px'/>
+        </div>
+      </div>
+
+    </div>
+
+    <div>
+      <img src='https://theme.hstatic.net/200000066044/1000820067/14/section_hot_banner.png?v=98' width='100%' />
+    </div>
+
+    <div className='py-4 '>
+      <h1 className='text-left hover:text-pink-400'>TIN TỨC</h1>
+      <div className='grid grid-cols-12 gap-3 pt-4'>
+        <div className='col-span-3'>
+          <div className='pl-9'>
+            <img   src='https://file.hstatic.net/200000066044/article/z2407320796082_b3dfecc6b8ef8ea74209aefc20127c54_de296ac8b0fa42fa86aab2b61ae46fae.jpg' width='300px'/>
+          </div>
+          <div className='text-left pt-2'>
+            
+            <h3 className='hover:text-pink-400'>TƯNG BƯNG SINH NHẬT LÊN 2 - NEW SKIN NHÂN ĐÔI ƯU ĐÃI</h3>
+            <h6 className='text-gray-400'>hdeide</h6>
+            <p>Thỏa mãn hơn cơn khát mua sắm với chương trình hoành tráng nhất năm của New Skin. Trong đó ngày siêu sale đỉnh nhất cùng hàng trăm...</p>
+
+          </div>
+
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        
+
+      </div>
+    </div>
+    </div>
+    
   )
 }
 
