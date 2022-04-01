e096e27d0dc268d42d661bf33f37a2047c6519d0
thao02122002 <vant16039@gamil.com>
2022-04-01 10:58:00 +0700

    AdminLayout

--- src/pages/layouts/Admin/AdminLayout.tsx
+++ src/pages/layouts/Admin/AdminLayout.tsx
@@ -7,21 +7,39 @@ type Props = {}
 
 const AdminLayout = (props: Props) => {
   return (
-   <div>
-     <div className='grid grid-cols-12'>
-     <div className='col-span-2'>
-       <Dashboard />
-       </div>
-       <div className='col-span-10 grid grid-cols-10'>
-         <div className='col-span-5'></div>
-         <div className='col-span-5'></div>
+    <div>
+      <div className='grid grid-cols-12'>
+        <div className='col-span-2'>
+          <Dashboard />
+        </div>
+        <div className='col-span-10 grid grid-cols-10'>
+          <div className='col-span-5'>
+            <form>
+              <div className='grid grid-cols-5'>
+                <div className='col-span-1 pl-20'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+                </div>
+                <div className='col-span-4 text-left'> <input className='border border-gray-300 py-1 px-2' type='text' placeholder='Tìm kiếm' /></div>
+              </div>
 
-         </div>
-       <Outlet />
+
+
+            </form>
+          </div>
+          <div className='col-span-5'>
+            <h2>Xin chào</h2>
+          </div>
+          <Outlet />
+
+
+        </div>
        
-     </div>
-   </div>
-    
+
+      </div>
+    </div>
+
   )
 }
 
